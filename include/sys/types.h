fdaf400833a45543154872a26975d23a53486d90
hypnoticos <48882960+hypnoticos@users.noreply.github.com>
2020-01-04 21:16:58 +0000

    Copyright year update

--- include/sys/types.h
+++ include/sys/types.h
@@ -1,6 +1,6 @@
 //
 // HypnoticOS
-// Copyright (C) 2019  jk30
+// Copyright (C) 2019, 2020  jk30
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
